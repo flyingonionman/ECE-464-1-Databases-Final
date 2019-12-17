ef0d6e01b08af0841ede587e3a59ff8f39a32911
flyingonionman <39283458+flyingonionman@users.noreply.github.com>
2019-12-17 07:27:51 -0500

    minor tweak

--- std/src/server/server.js
+++ std/src/server/server.js
@@ -5,7 +5,7 @@ const con = require('./database');
 var cors = require('cors');
 
 var corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: 'https://localhost:3000',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
